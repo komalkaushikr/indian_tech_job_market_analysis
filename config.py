a5e9a1b417a473258a597871c450fb6df9472b51
komal <komalkaushikr@gmail.com>
2026-07-12 17:01:27 +0530

    Initial commit: data cleaning, EDA, train/test split

---
+++ config.py
@@ -0,0 +1,6 @@
+RAW_DATA = r"E:\Desktop\job market ds\indian_tech_jobs_2026.csv"
+TARGET_COL = "salary_tier"
+TEST_SIZE = 0.2
+RANDOM_STATE = 42
+TRAIN_DATA = "data/train.csv"
+TEST_DATA ="data/test.csv"
